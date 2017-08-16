2d5b9bc060520e67030fedd4108ab0e356785f37
darkmarbo <darkmarbo@163.com>
2017-08-16 11:09:27 +0800

    v1.0 ok

---
+++ py_dict_sort.py
@@ -0,0 +1,12 @@
+
+import sys
+import os
+
+dic = {'a':31, 'bc':5, 'c':3, 'asd':4, 'aa':74, 'd':0}
+dict= sorted(dic.iteritems(), key=lambda d:d[1], reverse = True)
+print dict
+#print dic.iteritems()
+
+dic = {'a':31, 'bc':5, 'c':3, 'asd':4, 'aa':74, 'd':0}
+dict= sorted(dic.iteritems(), key=lambda d:d[0])
+print dict
---
+++ readme.sh
@@ -0,0 +1,25 @@
+
+========================= python ===================================================
+
+py_word_count.py:
+     输入分词后的文件 统计词频 
+
+py_dict_sort.py:
+     dict 按照key和value值排序
+
+py_system_grep.py 
+     内部使用system 调用 shell的grep命令 
+
+py_file_diff.py
+     统计file1 中的哪些行 不在 file2 中
+
+py_get_wav_longth.py 
+     统计目录下 所有wav的总时长 
+
+========================== shell ==================================================
+
+sh_pre.sh
+    shell  目录下所有文件 使用sed处理
+
+
+
